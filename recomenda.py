65b9f49d9cfc46b4889bb009b4ffa6ca7d8ab745
Lucca Ceresa Zimmermann <lucca@verema.com.br>
2016-07-23 14:53:05 -0300

    Alteração nos comentários do Script

--- recomenda.py
+++ recomenda.py
@@ -3,7 +3,11 @@ from operator import itemgetter
 log = True
 
 produtos = ['sushi1', 'sushi2', 'sushi3', 'sushi4', 'sushi5', 'sushi6', 'sushi7', 'sushi8', 'sushi9']
-compras = [[0,2,3], [0,1,3], [0,3,4], [0,2,8]] ## os números corresponde ao índice da lista produtos, exemplo [0,2,3] = ['sushi1', 'sushi3', 'sushi4']
+## essa é a lista de produtos (input)
+
+compras = [[0,2,3], [0,1,3], [0,3,4], [0,2,8]] 
+## Esse é o dicionário com a relação de compras anteriores (Uma mesma compra conteve os produtos 0,2,3 e 0,1,3 e 0,3,4 e 0,2,8)
+## os números corresponde ao índice da lista produtos, exemplo [0,2,3] = ['sushi1', 'sushi3', 'sushi4']
 
 
 i = 0
